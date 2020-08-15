902eca9d5e26c04be6b5730e6bca659934de8da5
VottusCode <vottus@vott.us>
2020-08-15 18:33:03 +0200

    init

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig, RouterMode } from "vue-router";
+
+Vue.use(VueRouter);
+
+const mode: RouterMode = "history";
+
+const routes: RouteConfig[] = [
+  {
+    path: "/",
+    component: () => import("@/layouts/default.vue"),
+    children: [
+      {
+        path: "/",
+        name: "Home",
+        component: () => import("@/pages/index.vue"),
+      },
+    ],
+  },
+];
+
+const router = new VueRouter({
+  mode,
+  routes,
+  base: process.env.BASE_URL,
+});
+
+export default router;
